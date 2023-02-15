ebfba6cf2a7dbd91e713ae2224a6fa28efc6ae13
antocranga <antocranga@gmail.com>
2023-02-15 13:32:39 +0200

    DB setup

--- db/script.sql
+++ db/script.sql
@@ -10,8 +10,8 @@ create table users
     email      varchar(100)         null,
     password   varchar(100)         null,
     provider   int                  null,
-    created_at date                 not null,
-    updated_at date                 not null,
+    created_at datetime             not null,
+    updated_at datetime             not null,
     constraint email
         unique (email)
 );
@@ -32,13 +32,13 @@ create table projects
 (
     id          int unsigned auto_increment
         primary key,
-    title       varchar(50)  null,
-    description varchar(150) null,
-    image_url   varchar(50)  null,
-    owner       int unsigned not null,
-    api_key     varchar(36)  not null,
-    created_at  date         not null,
-    updated_at  date         not null,
+    title       varchar(50)                  null,
+    description varchar(150)                 null,
+    owner       int unsigned                 not null,
+    api_key     varchar(36)                  not null,
+    created_at  datetime                     not null,
+    updated_at  datetime                     not null,
+    color       varchar(7) default '#2196f3' not null,
     constraint api_key
         unique (api_key),
     constraint projects_users_id_fk
@@ -53,8 +53,8 @@ create table issues
     description   mediumtext                   not null,
     project_id    int unsigned                 not null,
     status        varchar(15) default 'ACTIVE' not null,
-    created_at    date                         not null,
-    updated_at    date                         not null,
+    created_at    datetime                     not null,
+    updated_at    datetime                     not null,
     type          varchar(100)                 not null,
     message       mediumtext                   not null,
     is_bookmarked tinyint(1)  default 0        not null,
@@ -64,23 +64,32 @@ create table issues
 
 create table events
 (
-    id              varchar(36)  not null
+    id                    varchar(36)  not null
         primary key,
-    message         mediumtext   not null,
-    stack_trace     longtext     null,
-    body            text         null,
-    headers         text         null,
-    environment     varchar(15)  null,
-    package_name    varchar(30)  null,
-    package_version varchar(30)  null,
-    created_at      date         not null,
-    issue_id        int unsigned not null,
-    name            text         null,
-    type            text         not null,
-    version         varchar(30)  null,
-    user_agent      text         null,
+    message               mediumtext   not null,
+    stack_trace           longtext     null,
+    body                  text         null,
+    headers               text         null,
+    environment           varchar(15)  null,
+    package_name          varchar(30)  null,
+    package_version       varchar(30)  null,
+    created_at            datetime     not null,
+    issue_id              int unsigned not null,
+    name                  text         null,
+    type                  text         not null,
+    version               varchar(30)  null,
+    user_agent            text         null,
+    tags                  text         null,
+    project_id            int unsigned null,
+    device_type           text         null,
+    browser_major_version int          null,
+    browser               text         null,
+    platform_version      text         null,
+    browser_type          text         null,
     constraint events_issues_id_fk
-        foreign key (issue_id) references issues (id)
+        foreign key (issue_id) references issues (id),
+    constraint events_projects_id_fk
+        foreign key (project_id) references projects (id)
 );
 
 create index issues_project_id_index
